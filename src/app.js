447c8e0ecdbb6348e33921dbbcdac56b0e1eaf40
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-05-05 17:10:36 +0530

    added subscription controller

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ import videoRouter from "./routes/video.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter); // we have to use middleware  // sends control to user route
@@ -38,6 +39,8 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
 
 
 // url = http://localhost:8000/api/v1/users/register
---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,168 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { Subscription } from "../models/subscription.model.js"
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js"
+import { User } from "../models/user.model.js"
+import mongoose, { isValidObjectId } from "mongoose";
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+    // TODO: toggle subscription
+
+    if(!channelId || channelId.trim()=="" || !isValidObjectId(channelId)){
+        throw new ApiError(400, "invalid channel id");
+    }
+
+    const isSubscribed = await Subscription.find({
+        $and: [{channel: channelId}, {subscriber: new mongoose.Types.ObjectId(req.user?._id, {id: 1})}]
+    })
+
+    if(!isSubscribed){
+        throw new ApiError(500, "Something went wrong while toggleing")
+    }
+
+    if(isSubscribed.length === 0){
+        const addToSubscribed = await Subscription.create({
+            subscriber: req.user?._id,
+            channel: channelId
+        })
+
+        if(!addToSubscribed){
+            throw new ApiError("500", "something went wrong while adding to subscribed")
+        }
+
+        return res.status(200).json(
+            new ApiResponse(200, [], "added to subscribed")
+        )
+    }
+    else {
+        const toDelete = await Subscription.findByIdAndDelete(isSubscribed[0]._id);
+
+        if(!toDelete){
+            throw new ApiError(500, "something went rong while removing from subscription")
+        }
+
+        return res.status(200).json(
+            new ApiResponse(200, [], "removed from subscribed")
+        )
+    }
+})
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+
+    if(!channelId || channelId.trim() === "" || !isValidObjectId(channelId)){
+        throw new ApiError(400, "invalid channel id")
+    }
+
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId, {id: 1})
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscribers",
+
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullname: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $project: {
+                subscribers: 1
+            }
+        }
+    ])
+
+    if(!subscribers){
+        throw new ApiError(500, "something went wrong while fetching subscribers list")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, subscribers, "subscriber list fetched successfully")
+    )
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params // subscriber normal person fetching all subscribed channels
+
+    if(!subscriberId || subscriberId.trim() === "" || !isValidObjectId(subscriberId)){
+        throw new ApiError(400, "invalid user id")
+    }
+
+    const channelList = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId, {id: 1})
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channels",
+
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "subscriptions",
+                            localField: "_id",
+                            foreignField: "channel",
+                            as: "subscribers",
+                        }
+                    }, 
+                    {
+                        $addFields: {
+                            subscriberCount: {
+                                $size: "$subscribers"
+                            }
+                        }
+                    }, 
+                    {
+                        $project: {
+                            username: 1,
+                            fullname: 1,
+                            avatar: 1,
+                            coverimage: 1,
+                            subscriberCount: 1
+                        }
+                    }
+                ]
+            }
+        }, 
+        {
+            $project: {
+                channels: 1
+            }
+        }
+    ])
+
+    if(!channelList){
+        throw new ApiError(500, "something went wrong while fetching subscribed channel list")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, channelList, "subscribed channels fetched successfully")
+    )
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
